df0cc16f383c9455c36f6849fece5261aa7b51f8
derrkater <k.m.trojanowski@gmail.com>
2020-01-04 13:35:30 +0100

    Establish dice symbols.

---
+++ dice_symbols.py
@@ -0,0 +1,25 @@
+from abc import ABC
+
+
+class Symbol(ABC):
+    pass
+
+
+class Crit(Symbol):
+    pass
+
+
+class Hit(Symbol):
+    pass
+
+
+class Surge(Symbol):
+    pass
+
+
+class Block(Symbol):
+    pass
+
+
+class Blank(Symbol):
+    pass
