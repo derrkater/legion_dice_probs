8c2af93c3042b0ac7f140c43f950ebb465c07a54
derrkater <k.m.trojanowski@gmail.com>
2020-02-04 20:47:43 +0100

    Implement attack douse.

---
+++ legion_dice_probs/stochastic_objects/attack_douse.py
@@ -0,0 +1,78 @@
+import abc
+from typing import List
+
+from legion_dice_probs.stochastic_objects import douse as dse
+from legion_dice_probs.stochastic_states import symbol as sym
+
+
+class AttackDouse(dse.Douse, abc.ABC):
+    pass
+
+
+class RedAttackDouse(AttackDouse):
+
+    def get_sides(self) -> List[sym.Symbol]:
+        return [
+            sym.Crit(),
+            sym.Surge(),
+            sym.Hit(),
+            sym.Hit(),
+            sym.Hit(),
+            sym.Hit(),
+            sym.Hit(),
+            sym.Blank(),
+        ]
+
+    @staticmethod
+    def get_rolled_douse_cls() -> type(dse.RolledDouse):
+        return RolledRedAttackDouse
+
+
+class RolledRedAttackDouse(RedAttackDouse, dse.RolledDouse):
+    pass
+
+
+class BlackAttackDouse(AttackDouse):
+
+    def get_sides(self) -> List[sym.Symbol]:
+        return [
+            sym.Crit(),
+            sym.Surge(),
+            sym.Hit(),
+            sym.Hit(),
+            sym.Hit(),
+            sym.Blank(),
+            sym.Blank(),
+            sym.Blank(),
+        ]
+
+    @staticmethod
+    def get_rolled_douse_cls() -> type(dse.RolledDouse):
+        return RolledBlackAttackDouse
+
+
+class RolledBlackAttackDouse(BlackAttackDouse, dse.RolledDouse):
+    pass
+
+
+class WhiteAttackDouse(AttackDouse):
+
+    def get_sides(self) -> List[sym.Symbol]:
+        return [
+            sym.Crit(),
+            sym.Surge(),
+            sym.Hit(),
+            sym.Blank(),
+            sym.Blank(),
+            sym.Blank(),
+            sym.Blank(),
+            sym.Blank(),
+        ]
+
+    @staticmethod
+    def get_rolled_douse_cls() -> type(dse.RolledDouse):
+        return RolledWhiteAttackDouse
+
+
+class RolledWhiteAttackDouse(WhiteAttackDouse, dse.RolledDouse):
+    pass
