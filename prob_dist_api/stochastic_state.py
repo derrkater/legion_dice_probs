ac90859341da2b7d3fef331bf3832c037f0aebca
derrkater <k.m.trojanowski@gmail.com>
2020-02-06 22:18:38 +0100

    Extend StochasticState interface.

--- prob_dist_api/stochastic_state.py
+++ prob_dist_api/stochastic_state.py
@@ -1,12 +1,21 @@
+import abc
 import fractions
 
 from prob_dist_api import probability_distribution as pd
 
 
-class StochasticState:
+class StochasticState(abc.ABC):
     def get_probability_distribution(self) -> pd.ProbabilityDistribution:
         return pd.ProbabilityDistribution(
             {
                 self: fractions.Fraction(1)
             }
         )
+
+    @abc.abstractmethod
+    def __eq__(self, other):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def __hash__(self):
+        raise NotImplementedError
