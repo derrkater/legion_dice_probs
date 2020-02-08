b6482d610eed66aa2df42c73733f7542d5286dc8
derrkater <k.m.trojanowski@gmail.com>
2020-02-08 10:07:08 +0100

    WIP individual probability distributions.
    * TODO: consider removing.

---
+++ legion_dice_probs/probability_distributions/aggregation_methods.py
@@ -0,0 +1,9 @@
+from legion_dice_probs.stochastic_objects import douse as dse
+from legion_dice_probs.stochastic_objects import dice_pool as dce_pool
+
+
+def aggregate_dice(
+        douse_1: dse.RolledDouse,
+        douse_2: dse.RolledDouse,
+) -> dce_pool.RolledDicePool:
+    pass
---
+++ legion_dice_probs/probability_distributions/douse_probability_distribution.py
@@ -0,0 +1,5 @@
+from prob_dist_api import probability_distribution as pd
+
+
+class DouseProbabilityDistribution(pd.ProbabilityDistribution):
+    pass
