8298f003aa808a1202411062aaa311126c95fa40
derrkater <k.m.trojanowski@gmail.com>
2020-02-08 11:46:03 +0100

    Remove unused.

--- legion_dice_probs/events/roll.py
+++ legion_dice_probs/events/roll.py
@@ -1,7 +1,6 @@
 import logging
 from typing import Union
 
-from legion_dice_probs.probability_distributions import douse_probability_distribution as dse_pd
 from legion_dice_probs.stochastic_objects import dice_pool as dce_pool
 from legion_dice_probs.stochastic_objects import douse as dse
 from prob_dist_api import event as ev
@@ -21,7 +20,7 @@ class Roll(ev.Event):
             object_: Union[
                 dse.Douse,
                 dce_pool.DicePool,
-                dse_pd.DouseProbabilityDistribution,
+                pd.ProbabilityDistribution,
             ]
     ) -> Union[
         pd.ProbabilityDistribution,
--- legion_dice_probs/probability_distributions/douse_probability_distribution.py
+++
@@ -1,5 +0,0 @@
-from prob_dist_api import probability_distribution as pd
-
-
-class DouseProbabilityDistribution(pd.ProbabilityDistribution):
-    pass
