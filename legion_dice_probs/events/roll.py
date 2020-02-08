c0d38d643fd3e12b93348c3d42e180908fa325fa
derrkater <k.m.trojanowski@gmail.com>
2020-02-08 10:06:41 +0100

    WIP events.

--- legion_dice_probs/events/roll.py
+++ legion_dice_probs/events/roll.py
@@ -1,12 +1,20 @@
+import logging
 from typing import Union
 
 from legion_dice_probs.probability_distributions import douse_probability_distribution as dse_pd
 from legion_dice_probs.stochastic_objects import dice_pool as dce_pool
 from legion_dice_probs.stochastic_objects import douse as dse
 from prob_dist_api import event as ev
+from prob_dist_api import probability_distribution as pd
 
 
 class Roll(ev.Event):
+
+    def __init__(
+            self,
+            n_dice: int = 1,
+    ):
+        self.n_dice: int = n_dice
 
     def on(
             self,
@@ -16,6 +24,12 @@ class Roll(ev.Event):
                 dse_pd.DouseProbabilityDistribution,
             ]
     ) -> Union[
-        dse_pd.DouseProbabilityDistribution,
+        pd.ProbabilityDistribution,
     ]:
-        raise NotImplementedError
+        if isinstance(object_, dse.Douse):
+            if self.n_dice != 1:
+                logging.warning(f'Trying to roll {self.n_dice} dice when {object_} is given.')
+            return object_.get_probability_distribution()
+        elif isinstance(object_, dce_pool.DicePool):
+            raise NotImplementedError
+
