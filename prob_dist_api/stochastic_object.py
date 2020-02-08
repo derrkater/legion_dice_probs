98bc350002026783b76164135fcb92e5a06bbc11
derrkater <k.m.trojanowski@gmail.com>
2020-02-08 10:22:22 +0100

    Fix typing.

--- prob_dist_api/stochastic_object.py
+++ prob_dist_api/stochastic_object.py
@@ -1,8 +1,8 @@
 import abc
+from typing import Hashable
 from typing import List
 
 from prob_dist_api import probability_distribution as pd
-from prob_dist_api import stochastic_state as st_state
 
 
 class StochasticObject(abc.ABC):
@@ -16,5 +16,5 @@ class StochasticObject(abc.ABC):
     def get_probability_distribution(self) -> pd.ProbabilityDistribution:
         return self._probability_distribution
 
-    def get_stochastic_states(self) -> List[st_state.StochasticState]:
+    def get_stochastic_states(self) -> List[Hashable]:
         return list(self.get_probability_distribution().as_dict.keys())
