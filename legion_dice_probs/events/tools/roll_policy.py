90c39f3ab23b4bd7be96711ab0fe30c851d11d4a
derrkater <k.m.trojanowski@gmail.com>
2020-03-22 08:52:20 +0100

    Implement is_rollable property of RollPolicy.

--- legion_dice_probs/events/tools/roll_policy.py
+++ legion_dice_probs/events/tools/roll_policy.py
@@ -1,7 +1,11 @@
 from abc import ABC
+from typing import Tuple
+from typing import Type
+from typing import Union
 
 from legion_dice_probs.stochastic_objects import attack_douse as att_dse
 from legion_dice_probs.stochastic_objects import defence_douse as def_dse
+from legion_dice_probs.stochastic_objects import douse as dse
 from legion_dice_probs.stochastic_states import symbol as sym
 
 
@@ -10,15 +14,47 @@ class RollPolicy(ABC):
             self,
             dice_number: int = None,
     ):
-        pass
+        self.dice_number = dice_number
 
     @classmethod
-    def get_rollable_symbols(cls):
+    def get_rollable_symbols(cls) -> Tuple[Type[sym.Symbol]]:
         raise NotImplementedError
 
     @classmethod
-    def get_rollable_dice(cls):
+    def get_rollable_dice(cls) -> Tuple[Type[dse.Douse]]:
         raise NotImplementedError
+
+    def is_rollable(
+            self,
+            object_: Union[
+                sym.Symbol,
+                dse.RolledDouse,
+                dse.Douse,
+            ],
+    ):
+        if isinstance(object_, sym.Symbol):
+            return any(
+                isinstance(
+                    object_,
+                    rerollable_symbol_cls,
+                ) for rerollable_symbol_cls in self.get_rollable_symbols()
+            )
+        if isinstance(object_, dse.RolledDouse):
+            return any(
+                (
+                        self.is_rollable(object_.symbol) and
+                        isinstance(object_.douse, rerollable_douse_cls)
+
+                ) for rerollable_douse_cls in self.get_rollable_dice()
+            )
+        if isinstance(object_, dse.Douse):
+            return any(
+                isinstance(
+                    object_,
+                    rollable_douse_cls,
+                ) for rollable_douse_cls in self.get_rollable_dice()
+            )
+        raise ValueError
 
 
 class RollPolicyAttack(RollPolicy):
@@ -70,4 +106,3 @@ class RollPolicyDefence(RollPolicy):
             sym.Blank,
             sym.Surge,
         )
-
