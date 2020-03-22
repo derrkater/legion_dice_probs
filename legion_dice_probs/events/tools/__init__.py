16fcfe163fd98942447bc4ecae5b5a03d98006d2
derrkater <k.m.trojanowski@gmail.com>
2020-03-22 11:29:37 +0100

    Add future tasks for events.

---
+++ legion_dice_probs/events/tools/__init__.py
@@ -0,0 +1,3 @@
+# TODO: policies share a lot of common code. Think about potential interface and it's role in event API.
+# TODO: All events act similarly on StochasticObject and on ProbabilityDistribution. Both internally and between event
+#  implementations. Try to refactor.
