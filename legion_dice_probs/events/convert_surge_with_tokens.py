b95c13fdca80c5c962ee72cb82c3d88afd581526
derrkater <k.m.trojanowski@gmail.com>
2020-04-18 17:00:29 +0200

    Fix need of specification of conversion limit in ConvertSurgeWithTokens.
    * Implement tests for invariance of classes with no tokens.

--- legion_dice_probs/events/convert_surge_with_tokens.py
+++ legion_dice_probs/events/convert_surge_with_tokens.py
@@ -2,6 +2,7 @@ import logging
 from typing import Optional
 from typing import Union
 
+from legion_dice_probs.events.tools import conversion_policy as conv_pol
 from legion_dice_probs.events import convert_symbols as conv_syms
 from legion_dice_probs.stochastic_objects import dice_pool as dce
 from legion_dice_probs.stochastic_objects import dice_pool_attack as dce_att
@@ -24,6 +25,12 @@ class ConvertSurgeWithTokens(conv_syms.ConvertSymbols):
     #
     #     super().mark_conversion()
 
+    # def __init__(self, conversion_policy: conv_pol.ConversionPolicy):
+    #     super().__init__(
+    #         conversion_policy,
+    #         conversion_limit=None,
+    #     )
+
     def on(
             self,
             object_: Union[
@@ -39,11 +46,21 @@ class ConvertSurgeWithTokens(conv_syms.ConvertSymbols):
         pd.ProbabilityDistribution
     ]:
         if isinstance(object_, dce_att.RolledDicePoolAttack):
+            self._conversion_limit = object_.n_surge_tokens
             converted_rolled_dice_pool = super().on(object_)
             return dce_att.RolledDicePoolAttack(
                 rolled_dice_counter=converted_rolled_dice_pool.rolled_dice_counter,
                 n_surge_tokens=object_.n_surge_tokens - self.n_converted,
                 n_aim_tokens=object_.n_aim_tokens,
             )
+
+        if isinstance(object_, dce.RolledDicePool):
+            return object_.get_probability_distribution()
+
+        if isinstance(object_, dce_att.DicePoolAttack):
+            raise NotImplementedError
+
+        if isinstance(object_, dce.DicePool):
+            return object_.get_probability_distribution()
 
         return super().on(object_)
--- legion_dice_probs/tests/test_convert_surge_with_tokens.py
+++ legion_dice_probs/tests/test_convert_surge_with_tokens.py
@@ -2,6 +2,7 @@ import pytest
 
 from legion_dice_probs.stochastic_objects import douse as dse
 from legion_dice_probs.stochastic_objects import attack_douse as att_dse
+from legion_dice_probs.stochastic_objects import dice_pool as dce
 from legion_dice_probs.stochastic_objects import dice_pool_attack as dce_att
 from legion_dice_probs.stochastic_states import symbol as sym
 from legion_dice_probs.events import convert_surge_with_tokens as conv_srge_wtok
@@ -48,7 +49,6 @@ def test_convert_surge__on_rolled_dice_pool():
             convertible_symbols=(sym.Surge,),
             conversion_target=sym.Crit(),
         ),
-        conversion_limit=1,
     )
     assert action.on(rolled_dice_pool) == rolled_dice_pool_target
 
@@ -103,6 +103,72 @@ def test_convert_surge__on_rolled_dice_pool_attack__to_hit__single_dice_type(
             convertible_symbols=(sym.Surge,),
             conversion_target=sym.Hit(),
         ),
-        conversion_limit=n_surge_tokens,
     )
     assert action.on(rolled_dice_pool) == rolled_dice_pool_target
+
+
+@pytest.mark.parametrize(
+    'rolled_dice_list',
+    (
+            (
+                    [
+                        dse.RolledDouse(
+                            douse=att_dse.WhiteAttackDouse(),
+                            symbol=sym.Surge(),
+                        ),
+                    ]
+            ),
+            (
+                    [
+                        dse.RolledDouse(
+                            douse=att_dse.WhiteAttackDouse(),
+                            symbol=sym.Surge(),
+                        ),
+                        dse.RolledDouse(
+                            douse=att_dse.WhiteAttackDouse(),
+                            symbol=sym.Hit(),
+                        ),
+                    ]
+            ),
+    )
+)
+def test_convert_surge__on_rolled_dice_pool__no_surge_tokens(rolled_dice_list):
+    rolled_dice_pool = dce.RolledDicePool.from_rolled_dice_list(rolled_dice_list)
+    action = conv_srge_wtok.ConvertSurgeWithTokens(
+        conversion_policy=conv_pol.get_conversion_policy_attack(
+            convertible_symbols=(sym.Surge,),
+            conversion_target=sym.Hit(),
+        ),
+        conversion_limit=None,
+    )
+    assert action.on(rolled_dice_pool) == rolled_dice_pool.get_probability_distribution()
+
+
+@pytest.mark.parametrize(
+    'dice_list',
+    (
+            (
+                    [
+                        att_dse.RedAttackDouse(),
+                    ]
+            ),
+            (
+                    [
+                        att_dse.RedAttackDouse(),
+                        att_dse.RedAttackDouse(),
+                        att_dse.WhiteAttackDouse(),
+                        att_dse.WhiteAttackDouse(),
+                    ]
+            ),
+    )
+)
+def test_convert_surge__on_dice_pool__no_surge_tokens(dice_list):
+    dice_pool = dce.DicePool.from_dice_list(dice_list)
+    action = conv_srge_wtok.ConvertSurgeWithTokens(
+        conversion_policy=conv_pol.get_conversion_policy_attack(
+            convertible_symbols=(sym.Surge,),
+            conversion_target=sym.Hit(),
+        ),
+        conversion_limit=None,
+    )
+    assert action.on(dice_pool) == dice_pool.get_probability_distribution()
