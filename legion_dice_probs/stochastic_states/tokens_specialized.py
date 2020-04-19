d8756cdeaaade476e1298ea01a2e51aae8f28263
derrkater <k.m.trojanowski@gmail.com>
2020-04-19 09:45:48 +0200

    Implement specialized tokens pool connected with legion dice rolls.

---
+++ legion_dice_probs/stochastic_states/tokens_specialized.py
@@ -0,0 +1,44 @@
+from legion_dice_probs.stochastic_states import token as tok
+from legion_dice_probs.stochastic_states import tokens as toks
+
+
+class AimToken(tok.Token):
+    pass
+
+
+class DodgeToken(tok.Token):
+    pass
+
+
+class SurgeToken(tok.Token):
+    pass
+
+
+class TokensAttack(toks.Tokens):
+    @classmethod
+    def from_attack_tokens(
+            cls,
+            n_aim: int = 0,
+            n_surge: int = 0,
+    ):
+        return cls.from_symbols_list(
+            [
+                *[AimToken() for _ in range(n_aim)],
+                *[SurgeToken() for _ in range(n_surge)],
+            ]
+        )
+
+
+class TokensDefence(toks.Tokens):
+    @classmethod
+    def from_defence_tokens(
+            cls,
+            n_dodge: int = 0,
+            n_surge: int = 0,
+    ):
+        return cls.from_symbols_list(
+            [
+                *[DodgeToken() for _ in range(n_dodge)],
+                *[SurgeToken() for _ in range(n_surge)],
+            ]
+        )
